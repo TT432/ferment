8b3d23e8ab24b32f7bfd94a1125563e27876257a
TT432 <2437315224@qq.com>
2024-07-22 15:34:33 +0800

    :tada:

---
+++ src/main/java/io/github/tt432/ferment/Ferment.java
@@ -0,0 +1,11 @@
+package io.github.tt432.ferment;
+
+import net.neoforged.fml.common.Mod;
+
+/**
+ * @author TT432
+ */
+@Mod(Ferment.MOD_ID)
+public class Ferment {
+    public static final String MOD_ID = "ferment";
+}
